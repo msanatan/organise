43f13d65d072c05b36c7d923a82acef6bf3f6be1
Marcus Sanatan <marcussanatan@Marcuss-MacBook-Pro.local>
2014-10-26 22:15:01 -0400

    correct missing db import and correct url redirect after adding a todo

--- organise/views/todos.py
+++ organise/views/todos.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from organise.models import Todo
+from organise import db
 from flask import render_template, request, redirect, url_for, flash
 
 todos = Blueprint('todos', __name__, template_folder='/../templates')
@@ -16,7 +17,7 @@ def add_todo():
     db.session.add(todo)
     db.session.commit()
     flash('New todo was added!')
-    return redirect(url_for('index'))
+    return redirect(url_for('todos.index'))
 
 
 @todos.route('/<int:t_id>')
