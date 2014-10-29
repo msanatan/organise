61e4af18ddef3723b49bc4e6e7a8ff00e8a755af
Marcus Sanatan <marcussanatan@Marcuss-MacBook-Pro.local>
2014-10-28 22:51:49 -0400

    Add functionality for edit and delete functions

--- organise/views/todos.py
+++ organise/views/todos.py
@@ -16,7 +16,7 @@ def add_todo():
     todo = Todo(request.form['title'], request.form['description'])
     db.session.add(todo)
     db.session.commit()
-    flash('New todo was added!')
+    flash('New todo was added')
     return redirect(url_for('todos.index'))
 
 
@@ -26,11 +26,21 @@ def show_todo(t_id):
     return render_template('todo.html', todo=todo)
 
 
-@todos.route('/todo/<int:t_id>/edit')
+@todos.route('/todo/<int:t_id>/edit', methods=['POST'])
 def edit_todo(t_id):
-    pass
+    changed_title = request.form['title']
+    changed_description = request.form['description']
+    todo = Todo.query.filter_by(id=t_id).first()
+    todo.title = changed_title
+    todo.description = changed_description
+    db.session.commit()
+    flash('All changes were saved')
+    return redirect(url_for('todos.show_todo', t_id=t_id))
 
 
 @todos.route('/todo/<int:t_id>/delete', methods=['POST'])
 def delete_todo(t_id):
-    pass
+    todo = Todo.query.filter_by(id=t_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('todos.index'))
