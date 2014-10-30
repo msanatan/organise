4b29cc8e154455ed7b0ca379fada6a79bb91e599
Marcus Sanatan <marcussanatan@Marcuss-MacBook-Pro.local>
2014-10-29 22:06:57 -0400

    Clean up imports

--- organise/views/todos.py
+++ organise/views/todos.py
@@ -1,7 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from organise.models import Todo
 from organise import db
-from flask import render_template, request, redirect, url_for, flash
 
 todos = Blueprint('todos', __name__, template_folder='/../templates')
 
